10620f9e38bdeb0adc19e5c567729c6987237b6b
danielamaria <daniela.galatan28@gmail.com>
2021-02-23 15:39:19 +0200

    Using the 'get_object_or_404() method.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,32 +1,36 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
+
 
 # Create your views here.
 def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
 
+
 @login_required
 def topics(request):
     """Show all topics."""
-    topics = Topic.objects.filter(owner= request.user).order_by('date_added') # Restricting Topics Access to Appropriate Users
+    topics = Topic.objects.filter(owner=request.user).order_by(
+        'date_added')  # Restricting Topics Access to Appropriate Users
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
 
 @login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     # Make sure the topic belongs to the current user.
     if topic.owner != request.user:
-
         raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
 
 @login_required
 def new_topic(request):
@@ -46,6 +50,7 @@ def new_topic(request):
     # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
 
 @login_required
 def new_entry(request, topic_id):
@@ -68,6 +73,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+
 @login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
@@ -87,4 +93,3 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
-
